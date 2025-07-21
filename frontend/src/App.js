d1465dac07829a32057f7b0f7e5087c48a9ab1fa
YDLee-45 <corean24@naver.com>
2025-07-21 15:37:38 +0900

    Fix: remove wrong submodule reference to frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>하나로드 Step3: 연결 완료!</h1>
+      <p>React 앱이 정상적으로 실행되었습니다.</p>
+    </div>
+  );
+}
+
+export default App;
